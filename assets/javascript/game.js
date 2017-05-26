7277016b492d52e1e706452b419d903e4ab3bb13
lufherna <rojoclifford@gmail.com>
2017-05-26 00:24:18 -0400

    added inital jquery code and variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+$(function(){
+
+	//variables that hold the information for the game
+	var totalScore = 0;
+	var wins = 0;
+	var losses = 0;
+
+	//function that runs the program if the user wins or losses
+	function newRandom(){
+		//creates a random number between 19 and 120
+			var randomCompNumber = [Math.floor(Math.random() * 120) + 19];
+
+	}
+
+		newRandom();
+	function runProgram(){
+
+		//event handler that detects clicks
+
+
+		if (totalScore === randomCompNumber) {
+
+			wins++;
+			runProgram();
+
+			}
+
+		if (totalScore > randomCompNumber) {
+
+			losses++;
+			runProgram();
+
+			}
+
+
+} else {
+
+
+
+}
+
+
+})
