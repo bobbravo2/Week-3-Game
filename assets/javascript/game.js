b0067b4aa93923ab2a1dbe4433dc17e59586366e
lufherna <rojoclifford@gmail.com>
2017-05-27 20:59:34 -0400

    jquery external link isn't working. adding jquery to the html file for testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,24 @@
-$(function(){
-
+$(document).ready(function(){ 
 	//variables that hold the information for the game
 	var totalScore = 0;
 	var wins = 0;
 	var losses = 0;
 
 	//function that runs the program if the user wins or losses
-	function newRandom(){
-		//creates a random number between 19 and 120
-			var randomCompNumber = [Math.floor(Math.random() * 120) + 19];
 
-	}
-
-		newRandom();
 	function runProgram(){
 
 		//event handler that detects clicks
-		$("#crystal1").on("click" function() {
-
-
-
-
+		$(".candyPiece").on("click", function() {
+			var randomUserNumber = Math.floor(Math.random() * 12) + 1;
+			console.log(randomUserNumber)
 		})
 
 
 		if (totalScore === randomCompNumber) {
 
 			wins++;
+			newRandom();
 			runProgram();
 
 			}
@@ -36,14 +28,11 @@ $(function(){
 			losses++;
 			runProgram();
 
-			}
+			}   
 
 
-} else {
+		}
+
+	})
 
 
-
-}
-
-
-})
